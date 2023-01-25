59c1077e0b159d6ea107f7eb41ee504a755e1457
kboberg98 <kristian.boberg@no.experis.com>
2023-01-25 21:21:42 +0100

    Fixed styling for laptop resolution

--- src/components/Profile/ProfileHeader.jsx
+++ src/components/Profile/ProfileHeader.jsx
@@ -1,7 +1,9 @@
 const ProfileHeader = ({ username }) => {
     return (
         <header id="profile-header">
-            <h1 className="header-text">{ username }'s profile</h1>
+            <h3 className="header-text">{ username }'s profile</h3>
+            <hr />
+            <h5 className="header-text">Last 10 translations</h5>
         </header>
     )
 }
